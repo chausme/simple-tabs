79eb6455c98b20f3a2573e332374a8c2a3368542
Alex Chaus <chausme@gmail.com>
2022-12-12 14:54:48 +0400

    Add logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.nav-link');
+const articles = document.querySelectorAll('.about-content .content');
 
+about.addEventListener('click', e => {
+    const articleId = e.target.dataset.id;
+    if (articleId) {
+        btns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        e.target.classList.add('active');
+        articles.forEach(article => {
+            article.classList.remove('active');
+        });
+        const articleActive = document.getElementById(articleId);
+        articleActive.classList.add('active');
+    }
+});
